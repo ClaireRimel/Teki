2f9631b428505fb765b993512fc2cb84ea692184
ClaireRimel <sivadiercl@gmail.com>
2018-10-17 22:43:59 +0200

    OCR Teki

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var celebrities = ["le Patrick Sébastien", "le Laurent Gerra", "la Maria Carey", "la Céline Dion", "le Nicolás García"]
+    var activities = ["des pâtes", "de la Macarena", "des bêtises", "de la guitare","du mytho", "de la pétanque", "du tennis"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    // @IBOutlet weak var actionButton: UIButton!
+
+    @IBAction func changeQuote() {
+        let randomIndex1 = Int (arc4random_uniform(UInt32(celebrities.count)))
+        let celibrity = celebrities[randomIndex1]
+     
+        
+        let randomIndex2 = Int (arc4random_uniform(UInt32 (activities.count)))
+        let activity = activities [randomIndex2]
+        
+        let quote = "Tu es " + celibrity +  " " + activity + " !!!"
+        
+        quoteLabel.text = quote
+        
+        //actionButton.backgroundColor = .red
+        //actionButton.layer.cornerRadius = 16
     }
 
 
